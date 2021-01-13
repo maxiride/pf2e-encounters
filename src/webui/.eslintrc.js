8b92dbb4cecd5c23c51ea2deef477dcfa6ae1283
Federico D'Eredità <3630857+maxiride@users.noreply.github.com>
2021-01-13 23:45:01 +0100

    Update README.md

--- src/webui/.eslintrc.js
+++ src/webui/.eslintrc.js
@@ -61,6 +61,6 @@ module.exports = {
     // allow debugger during development only
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
 
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
   },
 };
