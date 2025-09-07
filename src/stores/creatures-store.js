3852049cbcdf91aa4c3db5f3076d1b676a3dbb9e
Federico D'Eredità <maxiride@gmail.com>
2025-09-07 14:30:24 +0200

    refactor(creatures-store): simplify metadata structure and fetching logic
    We no longer compute metadata values on the client but the app is provided with a metadata.json containing all of them.
    
    Removed `indexedProperties` and related compute methods. Added metadata fetching to streamline handling of creature metadata like sizes and traits. Enhanced getters for improved state access.

--- src/stores/creatures-store.js
+++ src/stores/creatures-store.js
@@ -3,40 +3,20 @@ import { defineStore, acceptHMRUpdate } from 'pinia'
 export const useCreaturesStore = defineStore('creatures', {
   state: () => ({
     isLoading: false,
-    indexedProperties: {
-      creature_families: {
-        field: 'creature_family',
-        uniqueValues: [],
-      },
-      sources: {
-        field: 'source',
-        uniqueValues: [],
-      },
-      rarities: {
-        field: 'rarity',
-        uniqueValues: [],
-      },
-      sizes: {
-        field: 'size',
-        uniqueValues: [],
-      },
-      traits: {
-        field: 'trait',
-        uniqueValues: [],
-      },
-      senses: {
-        field: 'sense',
-        uniqueValues: [],
-      },
-    },
-    properties: [],
+    // creatures hold the creature data
     creatures: [],
+    // metadata holds the creature metadata like sizes, traits etc
+    metadata: [],
   }),
 
   getters: {
     getCreatures: (state) => state.creatures,
-    getIndexedProperties: (state) => state.indexedProperties,
     getCreature: (state) => (index) => state.creatures.at(index),
+    getCreatureCount: (state) => state.creatures.length,
+    getTraits: (state) => state.metadata.traits,
+    getRarities: (state) => state.metadata.rarities,
+    getSizes: (state) => state.metadata.sizes,
+    getSources: (state) => state.metadata.sources_normalized,
   },
 
   actions: {
@@ -49,14 +29,22 @@ export const useCreaturesStore = defineStore('creatures', {
 
         // Validate response
         if (!response.ok) {
-          throw new Error(`Failed to fetch: HTTP ${response.status}`)
+          throw new Error(`Failed to fetch creatures: HTTP ${response.status}`)
         }
 
         // Parse JSON response and set creatures state
         this.creatures = await response.json()
 
-        console.log('Creatures loaded')
-        await this.computeUniqueValues()
+        console.log('Creatures loaded:', this.creatures.length)
+
+        const metadataResp = await fetch('/metadata.json')
+        if (!metadataResp.ok) {
+          throw new Error(`Failed to fetch metadata: HTTP ${metadataResp.status}`)
+        }
+
+        this.metadata = await metadataResp.json()
+
+        console.log('Metadata loaded')
       } catch (err) {
         // Handle errors (e.g., failed fetch, invalid JSON)
         this.error = err.message
@@ -64,47 +52,6 @@ export const useCreaturesStore = defineStore('creatures', {
       } finally {
         this.isLoading = false
       }
-    },
-
-    computeUniqueValues: async function () {
-      // Correctly bind `this` by using a regular function
-      try {
-        this.creatures.forEach((c) => {
-          Object.entries(this.indexedProperties).forEach(([key, value]) => {
-            if (!this.indexedProperties[key].uniqueValues.includes(c[value.field])) {
-              this.indexedProperties[key].uniqueValues.push(c[value.field])
-            }
-          })
-        })
-      } catch (error) {
-        console.error('Error computing creatures values:', error)
-      }
-    },
-
-    enumerateCreatureFields: function () {
-      const allKeys = new Set()
-
-      this.creatures.forEach((creature) => {
-        Object.keys(creature).forEach((key) => allKeys.add(key))
-      })
-
-      let a = Array.from(allKeys) // Returns a sorted array of unique keys
-
-      // All the keys are plain but some are logically grouped like resistance and weaknessess that are in the form of
-      // resistance.evil
-      // resistance.fire
-      // weakness.lawful
-      // weakness.mental
-
-      a.forEach((key) => {
-        const [rootKey, subKey] = key.split('.');
-        if (subKey) {
-          allKeys.add(rootKey);
-          allKeys[rootKey] = (allKeys[rootKey] || []).concat(subKey);
-        }
-      });
-
-      return allKeys
     },
   },
 })
